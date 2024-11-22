716611d724aa63e43fedcbba6f4c858bb3833a76
Nicolas <nicolas.migues97@gmail.com>
2024-11-22 03:28:10 -0300

    Detalles

--- Assets/Scripts/FinishTeleporter.cs
+++ Assets/Scripts/FinishTeleporter.cs
@@ -2,9 +2,14 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class FinishTeleporter : MonoBehaviour
 {
+    public GameObject Respawner;
+    private void Start()
+    {
+        Respawner = GameObject.Find("SpawnEnemiesFinish");
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && Respawner == null)
         {
             print(collision.name);
             SceneManager.LoadScene(2);
